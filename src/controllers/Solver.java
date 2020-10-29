4cb1a87c47d790b5beefbc9732013560ec94aac8
alexandre.vilanova <alexandre.vilanova@estudiantat.upc.edu>
2020-10-29 11:50:43 +0100

    add temporal implementation for getPossibleValues

--- src/controllers/Solver.java
+++ src/controllers/Solver.java
@@ -2,32 +2,113 @@ package src.controllers;
 
 import jdk.jshell.spi.ExecutionControl;
 import src.domain.BlackCell;
+import src.domain.Cell;
 import src.domain.Board;
+import src.domain.WhiteCell;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solver {
 	Board board;
-	int solutions;
+
+	ArrayList<Board> solutions;
 
 	public Solver(Board b) {
 		this.board = b;
-        this.solutions = -1;
+        this.solutions = new ArrayList<Board>();
     }
 
     public Solver(String formattedBoard) {
 		this.board = new Board(formattedBoard);
-        this.solutions = -1;
+        this.solutions = new ArrayList<Board>();
     }
 
     public void solve() {
-        solutions = 0;
         solve(0, 0);
     }
 
-    // TODO: Implement this function
+    // INFO: yet to be tested, also a in memory solution for handling possible values might be better
     private ArrayList<Integer> getPossibleValues(int row, int col) {
-	    return new ArrayList<>();
+        ArrayList<Integer> possibleValues = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+        int horizontalSum = 45;
+        int horizontalCurrentSum = 0;
+        int horizontalEmptyCount = 1;
+        for (int c = col - 1; c > 0; c--) {
+            Cell cell = board.getCell(row, c);
+            if (cell instanceof BlackCell) {
+                int sum = ((BlackCell) cell).getHorizontalSum();
+                if (sum != 0) {
+                    horizontalSum = sum;
+                    break;
+                }
+            } else { // cell is an instance of WhiteCell
+                int value = ((WhiteCell) cell).getValue();
+                if (value != 0) {
+                    horizontalCurrentSum += value;
+                    possibleValues.remove(value);
+                }
+                else horizontalEmptyCount++;
+            }
+        }
+        for (int c = col + 1; c < board.getWidth(); c++) {
+            Cell cell = board.getCell(row, c);
+            if (cell instanceof BlackCell) {
+                int sum = ((BlackCell) cell).getHorizontalSum();
+                if (sum != 0) break;
+            } else { // cell is an instance of WhiteCell
+                int value = ((WhiteCell) cell).getValue();
+                if (value != 0) {
+                    horizontalCurrentSum += value;
+                    possibleValues.remove(value);
+                }
+                else horizontalEmptyCount++;
+            }
+        }
+        for (int value : possibleValues) // TODO: use horizontalEmptyCount to make this part smarter
+            if (value + horizontalCurrentSum > horizontalSum)
+                possibleValues.remove(value);
+
+        int verticalSum = 45;
+        int verticalCurrentSum = 0;
+        int verticalEmptyCount = 1;
+        for (int r = row - 1; r > 0; r--) {
+            Cell cell = board.getCell(r, col);
+            if (cell instanceof BlackCell) {
+                int sum = ((BlackCell) cell).getVerticalSum();
+                if (sum != 0) {
+                    verticalSum = sum;
+                    break;
+                }
+            } else { // cell is an instance of WhiteCell
+                int value = ((WhiteCell) cell).getValue();
+                if (value != 0) {
+                    verticalCurrentSum += value;
+                    possibleValues.remove(value);
+                }
+                else verticalEmptyCount++;
+            }
+        }
+        for (int r = row + 1; r < board.getHeight(); r++) {
+            Cell cell = board.getCell(r, col);
+            if (cell instanceof BlackCell) {
+                int sum = ((BlackCell) cell).getHorizontalSum();
+                if (sum != 0) break;
+            } else { // cell is an instance of WhiteCell
+                int value = ((WhiteCell) cell).getValue();
+                if (value != 0) {
+                    verticalCurrentSum += value;
+                    possibleValues.remove(value);
+                }
+                else verticalEmptyCount++;
+            }
+        }
+        for (int value : possibleValues) // TODO: use verticalEmptyCount to make this part smarter
+            if (value + verticalCurrentSum > verticalSum)
+                possibleValues.remove(value);
+
+        return possibleValues;
     }
 
     private void solve(int row, int col) {
@@ -42,20 +123,17 @@ public class Solver {
         }
 
         if (board.getCell(row, col) instanceof BlackCell) {
-            // If cell type is black, continue solving
-            solve(row, col+1);
+            solve(row, col + 1); // If cell type is black, continue solving
             return;
         }
 
-        // NOTE: IntelliJ complains about unhandled exception. Ignore for now until getPossibleValues()
-        // is properly implemented.
         ArrayList<Integer> possibleValues = getPossibleValues(row, col);
 
         for (int i : possibleValues) {
             board.setCellValue(row, col, i);
             solve(row, col + 1);
             board.setCellValue(row, col, 0); // Setting it to 0 counts as empty
-            if (solutions > 1) return;
+            if (solutions.size() > 1) return;
         }
         
     }
