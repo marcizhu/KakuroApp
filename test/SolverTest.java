4bae0e55f3232f00b3b096f5a64517faa2a324c3
marcizhu <marc.izquierdo@est.fib.upc.edu>
2020-11-11 20:42:59 +0100

    Updated tests

--- test/SolverTest.java
+++ test/SolverTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.io.TempDir;
 import src.controllers.Reader;
-import src.controllers.Writer;
 import src.controllers.Solver;
 import src.domain.Board;
 
@@ -31,7 +30,6 @@ class SolverTest {
     void SolveSample() throws IOException {
         String inputFile = "data/sample.kak";
         String expectedOutputFile = "data/sample_solution0.kak";
-        String testOutputFile = tempDir.toString() + "/sample_solution0.kak";
         
         Reader r = new Reader(inputFile);
         Board b = r.read();
@@ -44,14 +42,10 @@ class SolverTest {
 
         Board solution = solver.getSolutions().get(0);
 
-        Writer writer = new Writer(testOutputFile);
-        writer.write(solution);
-
         File expectedOutput = new File(expectedOutputFile);
-        File testOutput = new File(testOutputFile);
 
         byte[] f1 = Files.readAllBytes(expectedOutput.toPath());
-        byte[] f2 = Files.readAllBytes(testOutput.toPath());
+        byte[] f2 = (solution.toString() + "\n").getBytes();
 
         assertTrue(Arrays.equals(f1, f2));
     }
