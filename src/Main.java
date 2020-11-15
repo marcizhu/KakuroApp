d185a29c1d1e3d8bd2742c972e805c13ed0155ea
Xavi Gordillo <xavier.gordillo@est.fib.upc.edu>
2020-11-15 20:17:22 +0100

    Added display for kakuros

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 package src;
 
+import src.controllers.Generator;
 import src.controllers.Solver;
 import src.controllers.Reader;
 import src.domain.Board;
@@ -7,6 +8,9 @@ import src.domain.Board;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import src.domain.Difficulty;
+import src.gui.KakuroGUI;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -17,19 +21,11 @@ public class Main {
 		int s = solver.solve();
 		System.out.println("\nSolutions found: " + s + "\n");
 
-		for (int i = 0; i < s; i++) {
-			Board solution = solver.getSolutions().get(i);
-			printBoard(solution);
-			System.out.println();
+		window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+		window.setLocationRelativeTo(null);
+		window.setSize(1200, 1200);
+		window.setVisible(true);
 
-			try {
-				PrintWriter serializer = new PrintWriter("data/kakuros/solved/two-sol-" + (i+1) + ".kak");
-				serializer.println(solution.toString());
-				serializer.close();
-			} catch(FileNotFoundException e) {
-				System.err.println("An error occurred while writing to file");
-			}
-		}
 	}
 
 	// For debugging purposes only
