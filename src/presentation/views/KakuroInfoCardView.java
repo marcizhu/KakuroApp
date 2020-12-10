f73843de49d2890e82654f73c8c007f37ab69d62
Alex Vilanova <alexandre.vilanova@estudiantat.upc.edu>
2020-12-10 13:13:11 +0100

    Stop using DomainCtrl.getMyKakurosList

--- src/presentation/views/KakuroInfoCardView.java
+++ src/presentation/views/KakuroInfoCardView.java
@@ -2,8 +2,6 @@ package src.presentation.views;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.sql.Timestamp;
 
 public class KakuroInfoCardView extends JPanel {
