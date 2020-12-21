6c40da46a908ab0056e644fadd885a00b281d500
Xavi Gordillo <xavier.gordillo@est.fib.upc.edu>
2020-12-21 20:10:25 +0100

    Remove score from User

--- src/domain/entities/User.java
+++ src/domain/entities/User.java
@@ -2,27 +2,17 @@ package src.domain.entities;
 
 public class User {
     private final String name;
-    private int score;
 
     public User(String name) {
         this.name = name;
-        this.score = 0;
     }
 
     public String getName() {
         return this.name;
     }
 
-    public int getScore() {
-        return this.score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
     public String toString() {
-        return "name: " + name + ", score: " + score;
+        return "name: " + name;
     }
 
 }
--- test/repository/UserRepositoryDBTest.java
+++ test/repository/UserRepositoryDBTest.java
@@ -71,32 +71,6 @@ public class UserRepositoryDBTest {
         verify(dbMock).readAll(User.class);
         verify(dbMock).writeToFile(expectedUsers, "User");
     }
-
-    @Test
-    public void testUpdateUser() throws IOException {
-        DB dbMock = mock(DB.class);
-        User user = new User("Larry");
-        user.setScore(420);
-
-        ArrayList<Object> expectedUsers = new ArrayList<>();
-        expectedUsers.add(user);
-
-        // DB returns Larry with a score of 420
-        when(dbMock.readAll(User.class)).thenReturn(expectedUsers);
-
-        UserRepository repo = new UserRepositoryDB(dbMock);
-
-        // We increase Larry's score by 5 and save it
-        user.setScore(425);
-        repo.saveUser(user);
-
-        expectedUsers = new ArrayList<>();
-        expectedUsers.add(user);
-
-        verify(dbMock).readAll(User.class);
-        // Therefore, we expect the database to write the updated Larry with score 425
-        verify(dbMock).writeToFile(expectedUsers, "User");
-    }
 }
 
 
