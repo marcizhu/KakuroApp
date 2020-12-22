ecb5710ebea07e9f6b4260777937e62aa58a57db
Alex Vilanova <alexandre.vilanova@estudiantat.upc.edu>
2020-12-22 13:57:20 +0100

    Invert kakurolist order + fix toppointer bug

--- src/domain/controllers/GameCtrl.java
+++ src/domain/controllers/GameCtrl.java
@@ -101,8 +101,8 @@ public class GameCtrl {
     private Map<String, Object> getTopPointerFromGameList(ArrayList<Game> games) {
         Game topPointer = null;
         for (Game game : games) {
-            if (game instanceof GameFinished && (topPointer == null || !((GameFinished) game).isSurrendered() &&
-                    ((GameFinished) game).getScore() > ((GameFinished)topPointer).getScore())) {
+            if (game instanceof GameFinished && (!((GameFinished) game).isSurrendered() && ( topPointer == null ||
+                    ((GameFinished) game).getScore() > ((GameFinished)topPointer).getScore()))) {
                 topPointer = game;
             }
         }
