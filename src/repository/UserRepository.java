77c28ab047fc1786d3eecd3504c6818deccfd414
Xavi Gordillo <xavier.gordillo@est.fib.upc.edu>
2020-12-22 13:26:25 +0100

    Javadoc for UserRepository

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -6,8 +6,32 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public interface UserRepository {
+    /**
+     * Returns a User with a given name
+     * @throws IOException if an unexpected problem occurs when reading from the database
+     * @param userName  Name that tidentifies the user that we want to get
+     * @return the User with the given name or null if it does not exist
+     */
     User getUser (String userName) throws IOException;
+
+    /**
+     * Deletes a user
+     * @throws IOException if an unexpected problem occurs when reading from the database
+     * @param user  The user that will be deleted
+     */
     void deleteUser (User user) throws IOException;
+
+    /**
+     * Saves a user
+     * @throws IOException if an unexpected problem occurs when reading from the database
+     * @param user  The user that will be saved
+     */
     void saveUser (User user) throws IOException;
+
+    /**
+     * Returns all users that have been saved
+     * @throws IOException if an unexpected problem occurs when reading from the database
+     * @return ArrayList of User objects with all the users that have been persisted
+     */
     ArrayList<User> getAllUsers () throws IOException;
 }
